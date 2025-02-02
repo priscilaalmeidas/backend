9db8410316737c3910f29545054b0ad6fb65f258
Priscila de Almeida <prisciladealmeida@outlook.com>
2025-02-02 18:16:55 -0300

    feat: edit CORS

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors();
 
   await app.listen(3000);
 }
