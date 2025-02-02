ad604669024907f388ac4afc07889dee4f72317f
Priscila de Almeida <prisciladealmeida@outlook.com>
2025-02-02 18:33:03 -0300

    feat: permitir todas as origens para CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'https://frontend-five-lime-25.vercel.app',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
