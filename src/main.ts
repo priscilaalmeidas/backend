8fdb0c88d256a0a307dc651f7f91520b06fcd07f
Priscila de Almeida <prisciladealmeida@outlook.com>
2025-02-02 18:10:24 -0300

    feat: config main to vercel

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOptions: CorsOptions = {
-    origin: ['https://frontend-five-lime-25.vercel.app/'],
+  app.enableCors({
+    origin: 'https://frontend-five-lime-25.vercel.app',
     credentials: true,
-  };
-
-  app.enableCors(corsOptions);
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await app.listen(3000);
 }
